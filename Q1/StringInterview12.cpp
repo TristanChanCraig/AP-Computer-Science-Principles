5269d0e4b1a82f7d2f6aaebf6ac517e8eadeb0fb
TristanChanCraig <126594566+TristanChanCraig@users.noreply.github.com>
2024-09-19 15:31:46 +0000

    9/19/24

--- Q1/StringInterview12.cpp
+++ Q1/StringInterview12.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
-void reverse(string& s, int i, int j) {
-    while (i < j) {
-        swap(s[i], s[j]);
-        i++;
-        j--;
+void reverse(string& s) {
+    for (int i = 0; i < s.length()-1; i++) {
+        for (int j = 0; j < s.length()-1; j++) {
+            if (s[j] == ' ') {
+                
+            }
+        }
     }
 }
 
 int main() {
     string sen;
-    printf("First number: ");
-    scanf("%s", &sen);
-
-    int j = 0;
-    sen.insert(sen.end(), ' ');
-    for (int i = 0; i < sen.length(); i++) {
-        if (sen[i] == ' ') {
-            reverse(sen, j, i - 1);
-            j = i + 1;
-        }
-    }
-
-    cout << sen;
+    printf("Enter sentence: ");
+    getline(cin, sen);
+    
+    cout << sen;    
 }
