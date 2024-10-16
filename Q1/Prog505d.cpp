1a8cbf0afd0051a0a6d72c3502c6a30c227a612c
TristanChanCraig <126594566+TristanChanCraig@users.noreply.github.com>
2024-10-16 15:47:20 +0000

    10/16/24

---
+++ Q1/Prog505d.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+
+class Prog505d {
+    private:
+        string first;
+        string last;
+        double hD1;
+        double bD1;
+        double hD2;
+        double bD2;
+        double hD3;
+        double bD3;
+        double hD4;
+        double bD4;
+        double hD5;
+        double bD5;
+        double hD6;
+        double bD6;
+        double hD7;
+        double bD7;
+    public:
+        Prog505d(string f, string l, double su, double bSu, double m, 
+        double bM, double tu, double bTu, double w, double bW, double th, 
+        double bTh, double f, double bF, double sa, double bSa) {
+            first = f;
+            last = l;
+            hD1 = su;
+            hD2 = m;
+            hD3 = tu;
+            hD4 = w;
+            hD5 = th;
+            hD6 = f;
+            hD7 = sa;
+        }
+
+
+};
+
+int main() {
+
+}
