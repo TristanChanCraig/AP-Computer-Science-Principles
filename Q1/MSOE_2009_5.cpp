f293ee204869cc7aad9aab3a18084b48ce7f3650
TristanChanCraig <126594566+TristanChanCraig@users.noreply.github.com>
2024-10-18 15:29:35 +0000

    10/18/24

--- Q1/MSOE_2009_5.cpp
+++ Q1/MSOE_2009_5.cpp
@@ -3,13 +3,30 @@
 #include <stdio.h>
 using namespace std;
 
-bool isPrime(num) {
-    for (int i = 2; i < num; i++) {
-        if (num % i == 0) return false;
+bool isPrime(int num) {
+    if (num <= 1) return false;
+    else {
+        int cnt = 0;
+        for (int i = 1; i <= num; i++) if (num % i == 0) cnt++;
+        if (cnt > 2) return false;
+        else return true;
     }
-    return true;
 }
 
 int main() {
-    
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    cout << "Is " << num << " prime? " << boolalpha << isPrime(num);
 }
+/*
+Enter a number: 65
+Is 65 prime? false
+
+Enter a number: 24
+Is 24 prime? false
+
+Enter a number: 7
+Is 7 prime? true
+*/
