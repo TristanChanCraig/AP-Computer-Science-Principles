81a4eb035ff17aeaa9d0e7f74b7674c8fac982be
TristanChanCraig <126594566+TristanChanCraig@users.noreply.github.com>
2024-10-28 15:28:26 +0000

    10/28/24

--- Q1/Project1.cpp
+++ Q1/Project1.cpp
@@ -6,6 +6,40 @@
 using namespace std;
 
 // To-Do List App
+bool execute(vector<string> tasks, string choice) {
+    if (choice == "E") {
+        return false;
+    } else if (choice == "A") {
+        string task;
+        string temp;
+        cout << "Enter the task: ";
+        getline(cin, temp);
+        getline(cin, task);
+        tasks.push_back(task);
+        cout << endl;
+    } else if (choice == "R") {
+        for (int i = 0; i < tasks.size(); i++) {
+            cout << i << ": " << tasks[i] << endl;
+        }
+        cout << endl;
+        int index;
+        printf("Enter the task number you want to remove: ");            
+        cin >> index;
+        tasks.erase(tasks.begin() + index);
+        cout << endl;
+    } else if (choice == "C") {
+        for (int i = 0; i < tasks.size(); i++) {
+            cout << i << ": " << tasks[i] << endl;
+        }
+        int index;
+        printf("Enter the task number you want to complete: ");
+        cin >> index;
+        tasks[index] = tasks[index] + " (Completed)";
+        cout << endl;
+    } else cout << "Invalid Choice!" << endl << endl;
+    return true;
+}
+
 int main() {
     bool run = true;
     vector<string> tasks;
@@ -18,36 +52,7 @@ int main() {
         printf("E for exit, A for add, R for remove, or C for complete a task: ");
         cin >> choice;
 
-        if (choice == "E") {
-            run = false;
-        } else if (choice == "A") {
-            string task;
-            string temp;
-            cout << "Enter the task: ";
-            getline(cin, temp);
-            getline(cin, task);
-            tasks.push_back(task);
-            cout << endl;
-        } else if (choice == "R") {
-            for (int i = 0; i < tasks.size(); i++) {
-                cout << i << ": " << tasks[i] << endl;
-            }
-            cout << endl;
-            int index;
-            printf("Enter the task number you want to remove: ");
-            cin >> index;
-            tasks.erase(tasks.begin() + index);
-            cout << endl;
-        } else if (choice == "C") {
-            for (int i = 0; i < tasks.size(); i++) {
-                cout << i << ": " << tasks[i] << endl;
-            }
-            int index;
-            printf("Enter the task number you want to complete: ");
-            cin >> index;
-            tasks[index] = tasks[index] + " (Completed)";
-            cout << endl;
-        } else cout << "Invalid Choice!" << endl << endl;
+        run = execute(vector<string> tasks, choice);
     }
     cout << "Successfully Exited";
 }
